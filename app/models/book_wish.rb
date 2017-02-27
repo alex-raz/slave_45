1aa8dabfaec7097cf708df4eece1aa1c951d0e7d
alexey_razuvaev <r125644@gmail.com>
2017-02-27 18:45:08 +0700

    Improve validations

--- app/models/book_wish.rb
+++ app/models/book_wish.rb
@@ -1,6 +1,4 @@
 class BookWish < ApplicationRecord
-  validates :title, :book_url, :price_rub, presence: true
-
   def self.balance
     initial_balance = 6000
     spent_amount = sum(:price_rub)
@@ -11,4 +9,10 @@ class BookWish < ApplicationRecord
   def in_process?
     applied || performed
   end
+
+  validates_presence_of :title, message: 'не может быть пустым в нашем эксклюзивном сервисе'
+  validates_presence_of :book_url, message: 'не может быть пустым в нашем эксклюзивном сервисе'
+  validates_presence_of :price_rub, message: 'не может быть пустым в нашем эксклюзивном сервисе'
+  validates_numericality_of :price_rub, greater_than_or_equal_to: 1, message: 'вот не надо тут нулей и отрицательностей всяких!..'
+  validates_numericality_of :price_rub, less_than_or_equal_to: BookWish.balance, message: 'Слава, мы тебя любим, но не настолько сильно. Умерь свои хотелки немного.'
 end
